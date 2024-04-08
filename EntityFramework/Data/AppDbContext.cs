3d521c73f24f467ee4d8ed9e783d082017a74284
AmirastanM <amirastanshm@code.edu.az>
2024-04-09 00:15:25 +0400

    added home task

---
+++ EntityFramework/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using EntityFramework.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        public  DbSet<Setting> Settings { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("C:\\Program Files\\Microsoft SQL Server\\MSSQL16.SQLEXPRESS\\MSSQL;Database=EntityFrameworkPb101Db;Trusted_Connection=true;TrustServer");
+        
+        }
+
+    }
+}
